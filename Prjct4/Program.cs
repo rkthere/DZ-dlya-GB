df3a03d939bba8ca9e6cd17cd3609276bbb54f34
Vlad <rkt9021@mail.ru>
2022-09-17 19:30:46 +0300

    Добавили все файлы в репозиторий

--- Prjct4/Program.cs
+++ Prjct4/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.Clear();
-Console.WriteLine
+int m = 1;
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+for(int i = m; i <= n; i++)
+{
+    if(i%2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
+Console.ReadKey();
