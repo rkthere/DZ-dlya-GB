eb7ac6d8ebb88fdc157679494a608f778680801f
Vlad <rkt9021@mail.ru>
2022-09-17 18:32:00 +0300

    Добавили все файлы в репо

---
+++ Prjct1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine($"Большее: {a}");
+}
+else
+{
+    Console.WriteLine($"Большее: {b}");
+}
