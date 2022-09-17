82d15f90a0b43742dfb9a65ef61a220be5de040f
Vlad <rkt9021@mail.ru>
2022-09-17 18:44:38 +0300

    Добавили второй код, который принимает 3 числа и показыавет большее

---
+++ Prjct2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a>b & a>c)
+{
+    Console.WriteLine($"Большее: {a}");
+}
+if(b>a & b>c)
+{
+    Console.WriteLine($"Большее: {b}");
+}
+if(c>a & c>b)
+{
+    Console.WriteLine($"Большее: {c}");
+}
